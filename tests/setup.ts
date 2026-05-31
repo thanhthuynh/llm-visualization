586050b19e4a24ea9574302c786d26adbbc1343b
Thanh <thanhthuynh7@gmail.com>
2026-05-30 23:20:29 -0400

    chore: scaffold Vite + React + TS + Tailwind v4 with vitest and playwright

---
+++ tests/setup.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import '@testing-library/jest-dom/vitest'
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+
+afterEach(() => cleanup())
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  configurable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  }),
+})
+
+Element.prototype.scrollIntoView = function () {}
